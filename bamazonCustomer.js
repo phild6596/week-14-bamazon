9659bfdb2597f9678c38173b0e679999cc555c56
phild6596 <phild6596@gmail.com>
2018-02-20 09:56:05 -0800

    Finished the bamazon store customer functions, final push.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,42 +1,83 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'bamazon'
 });
+
 connection.connect((err) =>{
     if (err) throw err;
-    console.log("Connected!");
-})
-
-
-function bamazonStore(err, buyAllTheThings) {   
-connection.query('SELECT * FROM products', (err, rows) => {
-    if (err) throw err;
-    console.log('Data received from Db:\n');
-    console.log(rows);
 });
 
-}
-
-
-
-function buyAllTheThings() {
-inquirer
-    .prompt([
-        {
-            name:'purchase',
-            message:'What would you like to purchase?'
-        },
-        {
-            name:'buy',
-            message:'How many?'
-        }
-    ]).then(function (answer) {
-        console.log(`Item(s): ${answer.purchase}`);
-        console.log(`Quantity: ${answer.buy}`);
+function bamazonStore() {
+    connection.query('SELECT * FROM products', (err, rows) => {
+        if (err) throw err;
+        console.log('Welcome to our store, please choose from id number:\n');
+        console.log(rows);
+        searchBamazon();
     });
 }
+
+function startBamazon() {
+    inquirer.prompt([{
+        name: 'bamazon',
+        type: 'confirm',
+        message: 'Would you like to purchase anything from Bamazon?'
+    }]).then(function (answer) {
+        if (answer.bamazon) {
+            console.log('Welcome to Bamazon, please select from below: ');
+            bamazonStore();
+        } else {
+            console.log('Thank you for visiting, see you soon.');
+            connection.end((err) => {
+                if (err) throw err;
+                console.log('Connection Closed.');
+            });
+            return false;
+        }
+    })
+}
+
+function searchBamazon() {
+    inquirer
+        .prompt([
+            {
+                name: 'purchase',
+                message: 'What item number would you like to purchase?'
+            },
+            {
+                name: 'buy',
+                message: 'How many?'
+            }
+        ]).then(function (answer) {
+            var itemSearched = answer.purchase;
+            var itemsBought = answer.buy;
+            console.log(`Item(s): ${itemSearched}`);
+            console.log(`Quantity: ${itemsBought}`);
+            buyBamazon(itemSearched, itemsBought);
+        });
+}
+
+
+
+function buyBamazon(itemSearched, itemsBought) {
+    connection.query('SELECT * FROM  products WHERE id = ' + itemSearched, function (err, response) {
+        if (err) {
+            console.log(err);
+        }
+        if (itemsBought <= response[0].stock_quantity) {
+            var itemsCost = response[0].price * itemsBought;
+            console.log('Shipping right out, your cost is: $' + itemsCost + ', Thank you for your business!');
+            connection.query('UPDATE products SET stock_quantity = stock_quantity - ' + itemsBought + ' WHERE id = ' + itemSearched);
+        } else {
+            'Sorry, we are currently out of ' + response[0].product_name +', please check again sonn.'
+        }
+        startBamazon();
+    })
+}
+
+startBamazon();
